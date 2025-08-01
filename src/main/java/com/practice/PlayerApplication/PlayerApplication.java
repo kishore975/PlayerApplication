cd0a869f1480dfc5283ba41a625016019840db91
kishorekasim <kishorehgsk9@gamil.com>
2025-08-01 12:53:01 +0530

    second change

--- src/main/java/com/practice/PlayerApplication/PlayerApplication.java
+++ src/main/java/com/practice/PlayerApplication/PlayerApplication.java
@@ -9,6 +9,8 @@ public class PlayerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlayerApplication.class, args);
 		System.out.println("working");
+		
+		
 	}
 
 }
